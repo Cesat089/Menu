b74a6098a1726ff871f956b0b3c8a030255a6c74
César Eladio Cabello Afonso <cesare.cabelloa@gmail.com>
2022-12-13 14:10:33 +0100

    Commit Inicial

---
+++ Ejercicio9/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ejercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int option;
+            int num1 = 0;
+            int num2 = 0;
+
+            Console.WriteLine("Introduzca dos números para sus operaciones");
+        }
+    }
+}
